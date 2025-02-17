1d097c9ac88b8af16892cd097d11fb1d4a2a7e40
quit <nnooaahh24@gmail.com>
2025-02-17 14:34:38 +0100

    Database seed Test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,23 @@
-require 'csv'
+# require 'csv'
 
-csv_file = Rails.root.join('db', 'volcanoes.csv')
+# csv_file = Rails.root.join('db', 'volcanoes.csv')
 
-CSV.foreach(csv_file, headers: true) do |row|
-  Volcano.find_or_create_by(
-    name: row['Name'],
-    location: row['Location'],
-    country: row['Country'],
-    latitude: row['Latitude'].to_f,
-    longitude: row['Longitude'].to_f,
-    elevation: row['Elevation (m)'].to_i,  # Make sure column name matches exactly!
-    volcano_type: row['Type'],
-    vei: row['VEI'].to_i,  # Volcanic Explosivity Index (VEI)
-    deaths: row['Deaths'].to_i,
-    injuries: row['Injuries'].to_i,
-    damage: row['Damage ($Mil)'].to_f,
-    houses_destroyed: row['Houses Destroyed'].to_i,
-    total_deaths: row['Total Deaths'].to_i
-  )
-end
+# CSV.foreach(csv_file, headers: true) do |row|
+#   Volcano.find_or_create_by(
+#     name: row['Name'],
+#     location: row['Location'],
+#     country: row['Country'],
+#     latitude: row['Latitude'].to_f,
+#     longitude: row['Longitude'].to_f,
+#     elevation: row['Elevation (m)'].to_i,  # Make sure column name matches exactly!
+#     volcano_type: row['Type'],
+#     vei: row['VEI'].to_i,  # Volcanic Explosivity Index (VEI)
+#     deaths: row['Deaths'].to_i,
+#     injuries: row['Injuries'].to_i,
+#     damage: row['Damage ($Mil)'].to_f,
+#     houses_destroyed: row['Houses Destroyed'].to_i,
+#     total_deaths: row['Total Deaths'].to_i
+#   )
+# end
 
-puts "Seeded #{Volcano.count} volcanoes!"
+# puts "Seeded #{Volcano.count} volcanoes!"
