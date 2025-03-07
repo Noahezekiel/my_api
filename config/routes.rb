c94fca4e548ffd5a6788cde13c3eae8a3453db73
quit <nnooaahh24@gmail.com>
2025-03-07 11:57:59 +0100

    Uncommented Devise routes for user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: proc { [200, { "Content-Type" => "application/json" }, ['{ "message": "Welcome to My API" }']] }
   use_doorkeeper
-  # devise_for :users
+  devise_for :users
 
   namespace :api do
     resources :movies, only: [:index, :show, :create, :update, :destroy]
