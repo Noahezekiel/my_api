b9826a8627f69b6b04c028f98363f72aa96744ab
quit <nnooaahh24@gmail.com>
2025-03-18 17:41:49 +0100

    still Fix port binding for Render

--- config/puma.rb
+++ config/puma.rb
@@ -2,9 +2,11 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3).to_i
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", "3000").to_i
+# Replace this line:
+port ENV.fetch("PORT", 3000).to_i
 
+# With this (no fallback):
+port ENV.fetch("PORT")
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
