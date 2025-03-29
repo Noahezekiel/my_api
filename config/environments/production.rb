9d1dc4ccfdf878c135b04b5325690811e08c6fc8
quit <nnooaahh24@gmail.com>
2025-03-29 18:12:14 +0100

    Fix: Initialize Logger Before Logging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
   config.action_cable.mount_path = nil
   config.action_cable.disable_request_forgery_protection = true
   config.action_cable.allowed_request_origins = []
+  Rails.logger = Logger.new(STDOUT) unless Rails.logger
   Rails.logger.info "DATABASE_URL: #{ENV['DATABASE_URL']}"
 
   
